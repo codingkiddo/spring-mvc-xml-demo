bc903dc4ac0772911d8a50080c815512d996c684
codingkiddo <codingkiddo@gmail.com>
2023-04-06 11:36:19 +0530

    Initial Commit

---
+++ src/main/java/com/spring/quickstarts/controllers/AdminController.java
@@ -0,0 +1,13 @@
+package com.spring.quickstarts.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+  @RequestMapping("/admin")
+  public String showAdminPage() {
+	  return "home";
+  }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+
+
+<div>
+  <h2>A global community of friends and strangers spitting out their 
+  inner-most and personal thoughts on the web for everyone else to 
+  see.</h2>
+  <h3>Look at what these people are spitting right now...</h3>
+
+
+</div>
